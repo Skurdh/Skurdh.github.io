5d29a7afae0335787cd95960a3c0c69ebbc1e0cc
Sku <vinegraphe@gmail.com>
2023-01-15 02:26:19 +0100

    Update Linux page

--- minihub/obs.html/static/obsidian_core.js
+++ minihub/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/minihub";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '69524055517503890906009933088400642356'                       // used to check whether the localStorage data is stale
+var gzip_hash = '292344738529566019566507381899387982794'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
