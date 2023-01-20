4cdf987d7200e7677275cc6a3179ebbe1275a999
Sku <vinegraphe@gmail.com>
2023-01-20 03:11:46 +0100

    Update doc

--- minihub/obs.html/static/obsidian_core.js
+++ minihub/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/minihub";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '200787317124817310118600557953499121029'                       // used to check whether the localStorage data is stale
+var gzip_hash = '52961633725240494415707834462615635625'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
